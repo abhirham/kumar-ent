b6821c3f908806499a85fab2f9f48952d14e6b77
abhirham94 <abhirham@skynetsoftware.ca>
2025-02-10 07:14:22 -0500

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-import Navbar from "./components/Navbar.vue";
-import "./assets/main.css";
-</script>
-
 <template>
     <div class="my-5 mx-auto container">
         <Navbar />
         <main class="m-5">
-            <RouterView />
+            <Login v-if="showLogin" />
+            <RouterView v-else />
         </main>
     </div>
 </template>
+
+<script>
+import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+import "./assets/main.css";
+import { auth } from "@/libs/firebase";
+import Login from "./views/Login.vue";
+
+export default {
+    components: { Login },
+    data() {
+        return {
+            showLogin: true,
+        };
+    },
+    mounted() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                this.showLogin = false;
+                console.log("user logged in");
+            } else {
+                this.showLogin = true;
+                console.log("user not logged in");
+            }
+        });
+    },
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <Card>
+        <template #title>Kumar ENT</template>
+        <template #subtitle>Login to continue</template>
+        <template #content>
+            <div class="flex flex-col gap-3">
+                <FloatLabel variant="in">
+                    <InputNumber
+                        v-model="value2"
+                        inputId="withoutgrouping"
+                        :useGrouping="false"
+                        fluid
+                    />
+                    <label for="withoutgrouping">Phone #</label>
+                </FloatLabel>
+                <FloatLabel variant="in">
+                    <Password inputId="password" :feedback="false" fluid />
+                    <label for="password">Password</label>
+                </FloatLabel>
+                <Button fluid label="Continue" />
+            </div>
+        </template>
+    </Card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
