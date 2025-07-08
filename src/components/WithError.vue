4ec35720135fe0cce5a638a8c75de51c49273440
abhirham94 <abhirham@skynetsoftware.ca>
2025-07-07 23:20:17 -0400

    created reusable component

---
+++ src/components/WithError.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col gap-1">
+        <slot :invalid="errorToUse.$error"></slot>
+        <Message
+            v-if="errorToUse.$error"
+            severity="error"
+            size="small"
+            variant="simple"
+            >{{ errorToUse.$errors[0].$message }}</Message
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["error", "errorArr"],
+    computed: {
+        errorToUse() {
+            return (
+                this.error ?? {
+                    $error: this.errorArr.length > 0,
+                    $errors: this.errorArr,
+                }
+            );
+        },
+    },
+};
+</script>
